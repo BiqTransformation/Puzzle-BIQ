ded73aafb5f4e823359e58122290b456c6113ed5
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-03-31 23:27:34 +0300

    [FileOutput] --------------------- fixing the FileOutput class to get only a string without file name

--- Puzzle-BIQ/src/main/java/course/puzzle/puzzle/Parameters.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/puzzle/Parameters.java
@@ -8,9 +8,9 @@ public class Parameters {
     public static String CANNOT_SOLVE_PUZZLE = "Cannot solve puzzle:";
     public static String WRONG_NUMBER_OF_STRAIGHT_EDGES = "Wrong number of straight edges";
     public static String MISSING_CORNER_TL = "missing corner element: "+ element;
-    public static String MISSING_CORNER_TR = "Cannot solve puzzle: missing corner element: TR";
-    public static String MISSING_CORNER_BL = "Cannot solve puzzle: missing corner element: BL";
-    public static String MISSING_CORNER_BR = "Cannot solve puzzle: missing corner element: BR";
+    public static String MISSING_CORNER_TR = "missing corner element: TR";
+    public static String MISSING_CORNER_BL = "missing corner element: BL";
+    public static String MISSING_CORNER_BR = "missing corner element: BR";
     public static String MISSING_PUZZLE_ELEMENTS = "missing puzzle elements with the following IDs:";
     public static String WRONG_ELEMENT_ID = "Puzzle of size "+size + "cannot have the following IDs:";
     public static String WRONG_ELEMENT_FORMAT = "Puzzle ID" + id +"has wrong data:";   
--- Puzzle-BIQ/src/test/java/course/puzzle/file/OutputFileTests.java
+++ Puzzle-BIQ/src/test/java/course/puzzle/file/OutputFileTests.java
@@ -2,9 +2,12 @@ package course.puzzle.file;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 import org.junit.Test;
 
@@ -14,16 +17,32 @@ public class OutputFileTests {
 
 	@Test
 	public void WriteReadtoOutputFile() throws IOException{
+		//clean file
+		try{
+			PrintWriter writer = new PrintWriter("output.txt");
+			writer.print("");
+			writer.close();
+		}
+		catch(IOException e){
+			
+		}
+		
 		//save
-		FileOutputStream fos = new  FileOutputStream("output.txt");
 		String message = Parameters.CANNOT_SOLVE_PUZZLE;
-		FileOutput.saveAsTextFile(fos, message);
+		FileOutput.saveAsTextFile(message);		
+		//save again
+		String message2 = Parameters.MISSING_CORNER_BL;
+		FileOutput.saveAsTextFile(message2);
 		//load
-		FileInputStream fis = new FileInputStream("output.txt");
-		String message2 = FileOutput.loadFromTextFile(fis);
-		
-		assertEquals(message,message2);
-		
+		//message2 =message +"\n" + message2;
+		//System.out.println("message2 " + message2);
+		//System.out.println();
+		String message3 =  FileOutput.loadFromTextFile();
+		//System.out.println("message3 " + message3);
+		assertTrue(message3.contains("Cannot solve puzzle:"));
+		assertTrue(message3.contains("missing corner element: BL"));
 	}
+	
+	
 
 }
