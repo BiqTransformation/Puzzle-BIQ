be1ebde57e20a46289f64bbf0d570cdee50895dc
Alexander Coral <coral.alexander@gmail.com>
2018-04-06 10:14:38 +0300

    [Alex] class FileReaderWriterTest and fileDataValidator updated

--- Puzzle-BIQ/src/main/java/course/puzzle/file/FileDataValidation.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/file/FileDataValidation.java
@@ -1,139 +1,170 @@
 package course.puzzle.file;
 
+import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 
 import course.puzzle.puzzle.PuzzlePiece;
 
 public class FileDataValidation {
-
-    private int numOfPieces;
+    
+	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	private int numOfPieces;
 	private ArrayList<String> inPutlist = new ArrayList<>();
 	ArrayList<PuzzlePiece> listOfPuzzlePieces = new ArrayList<>();
-	ArrayList<PuzzlePiece> listOfPuzzlePiecesAfterAfterAllValidation = new ArrayList<>();
-	/** The method return list of puzzle pieces after all data from file was validated .
+	ArrayList<PuzzlePiece> listOfPuzzlePiecesAfterAllValidation = new ArrayList<>();
+
+	/**
+	 * The method return list of puzzle pieces after all data from file was
+	 * validated .
+	 * 
 	 * @param inPutlist
 	 * @return listOfPuzzlePieces
+	 * @throws IOException 
 	 */
-	public ArrayList<PuzzlePiece> fileDataValidator(ArrayList<String> inPutlist) {
+	public ArrayList<PuzzlePiece> fileDataValidator(ArrayList<String> inPutlist) throws IOException {
 		ArrayList<Integer> validSetOfIntegers = null;
-		
+		if (basicFileValidator(inPutlist)) {
+			try {
+				numOfPieces = firstLineValidator(inPutlist.get(0));
+				if (numOfPieces != -1) {
+
+					for (int i = 1; i < inPutlist.size(); i++) {
+
+						validSetOfIntegers = integersListValidation(inPutlist.get(i));
+
+						if (validSetOfIntegers.size() == 5) {  ///// Should be prepared !!!!!!!!!!!!!!!!!!!1
+							listOfPuzzlePiecesAfterAllValidation = PuzzlePieceBuilder(validSetOfIntegers);
+						} else {
+							FileOutput.printToOutputFile(timestamp+" : "+"Puzzle will not be complited , one or more parametrs are incorrect. ");
+						}
+					}
+				} 
+			} catch (Exception e) {
+
+				// TO DO write to Error File .
+			}
+		} else {
+			FileOutput.printToOutputFile(timestamp+ " : " + "Input list not valid !!!  ");
+		}
+
+		if (listOfPuzzlePiecesAfterAllValidation.size() == numOfPieces) {
+			return listOfPuzzlePiecesAfterAllValidation;
+		} else {
+			FileOutput.printToOutputFile(timestamp+ " : " + "number of puzzle pieces not equal to requared : "+numOfPieces +" actual : "+ listOfPuzzlePiecesAfterAllValidation.size() ); 
+			listOfPuzzlePiecesAfterAllValidation.clear();
+			return listOfPuzzlePiecesAfterAllValidation;
+		}
+	}
+
+	/**
+	 * firstLineValidator designed to verify the format of first line only !!! .
+	 * 
+	 * @param firstLine
+	 * @return integer with number of pieces in case of good scenario and -1 in case
+	 *         of bad scenario .
+	 * @throws IOException 
+	 */
+	protected int firstLineValidator(String firstLine) throws IOException {
+		int numOfPieces = -1;
+
+		String[] arrStr = firstLine.trim().replaceAll(" ", "").split("=");
+
+		if (arrStr[0].equals("NumElements")) {
+			try {
+				if (Integer.parseInt(arrStr[1]) > 1) {
+					numOfPieces = Integer.parseInt(arrStr[1]);
+					setNumOfPieces(numOfPieces);
+					return numOfPieces;
+
+				} else {	
+						FileOutput.printToOutputFile(timestamp+" : "+"The NumElements less then 2 current number is "+ arrStr[1]);
+					return numOfPieces;
+				
+				}
+			} catch (NumberFormatException e) {
+				e.printStackTrace();		
+					FileOutput.printToOutputFile(timestamp+" : "+"ParseInt failed , parameter is :"+ arrStr[1]);
+				return numOfPieces;
+			}
+		} else {
+			FileOutput.printToOutputFile(timestamp+" : "+"Format error , expected parameter is NumElements ,but actual is :"+ arrStr[0]);
+			return numOfPieces;
+			
+		}
+	}
+
+	/**
+	 * @param idNum
+	 * @return returnIdNum
+	 */
+	protected int idNumberValidation(int idNum) {
+		int returnIdNum = -1;
+		if (idNum <= numOfPieces && idNum > 0) {
+			returnIdNum = idNum;
+			return returnIdNum;
+		} else {
+			return returnIdNum;
+			// // TO DO write error to file .
+		}
+
+	}
+
+	/**
+	 * Every line include the 5 integers 1 number it's piece Id ,others 4 are sides
+	 * of puzzle.
+	 * 
+	 * @param str
+	 * @return listOfIntegers
+	 * @throws IOException 
+	 */
+	protected ArrayList<Integer> integersListValidation(String str) throws IOException {
+		int currNum = 0;
+		ArrayList<Integer> listOfIntegers = new ArrayList<>();
+
+		String[] afterSplit = str.trim().split(" ");
+
 		try {
+			int validId = idNumberValidation(Integer.parseInt(afterSplit[0]));
+			if (validId != -1) {
+				listOfIntegers.add(validId);
+				for (int i = 1; i < afterSplit.length; i++) {
+					if (afterSplit[i].equals("-1") || afterSplit[i].equals("0") || afterSplit[i].equals("1")) {
 
-			int numOfPieces = firstLineValidator(inPutlist.get(0));
+						try {
+							listOfIntegers.add(Integer.parseInt(afterSplit[i]));
 
-			if (numOfPieces != -1) {
-                
-				for (int i = 1; i < inPutlist.size(); i++) {
-
-					validSetOfIntegers = integersListValidation(inPutlist.get(i));
-
-					if (!validSetOfIntegers.isEmpty()) {
-						listOfPuzzlePiecesAfterAfterAllValidation = PuzzlePieceBuilder(validSetOfIntegers);
+						} catch (NumberFormatException e) {
+							e.printStackTrace();
+							FileOutput.printToOutputFile(timestamp+" : "+"ParseInt failed , parameter is :"+ afterSplit[i]);
+						}
 					} else {
-						
-						// TO DO write to Error File .
+						FileOutput.printToOutputFile(timestamp+"For Id : "+validId+" , parameter number :"+ i +" is incorrect : "+ afterSplit[i]);
+						//break;
 					}
 				}
 			} else {
-				// TO DO write to Error File .
+				FileOutput.printToOutputFile(timestamp+" Id number incorrect !!!");
+				listOfIntegers.remove(0);	
 			}
-		} catch (Exception e) {
 
-			// TO DO write to Error File .
-		}
-		
-		if (listOfPuzzlePiecesAfterAfterAllValidation.size() == numOfPieces) {
-			return listOfPuzzlePiecesAfterAfterAllValidation;
-		}
-		else {
-			listOfPuzzlePiecesAfterAfterAllValidation.clear();
-			return listOfPuzzlePiecesAfterAfterAllValidation;
-		}
+		} catch (NumberFormatException e) {
 			
-	}
-
-	/** firstLineValidator designed to verify the format of first line only .
-	 * @param firstLine
-	 * @return integer with number of pieces in case of good scenario and -1 in case of bag scenario .
-	 */
-	protected int firstLineValidator(String firstLine) {
-		int numOfPieces = 0;
-
-		String[] arrStr = firstLine.trim().split("=");
-
-		if (arrStr[0].equals("NumElements")) {
-			if (Integer.parseInt(arrStr[1]) > 1) {
-				numOfPieces = Integer.parseInt(arrStr[1]);
-			} else {
-				numOfPieces = -1;
-			}
-		} else {
-			numOfPieces = -1;
+			//FileOutput.saveAsTextFile(timestamp+" : "+"ParseInt failed , parameter is :"+ afterSplit[i]);
+			//listOfIntegers.clear();
 			// TO DO write error to file .
+			e.printStackTrace();
 		}
-		
-		setNumOfPieces(numOfPieces);
-		
-		return numOfPieces;
-	}
-	
-	protected int idNumberValidation(int idNum) {
-		int returnIdNum = 0 ;
-		if (idNum <=  numOfPieces && idNum > 0) {
-			returnIdNum = idNum ;
-		}
-		else {
-			returnIdNum = -1;
-			//  // TO DO write error to file .
-		}
-		return returnIdNum;	
-	}
 
-	/** Every line include the 5 integers 1 number it's piece Id ,others  4 are sides of puzzle.
-	 * 
-	 * @param str
-	 * @return
-	 */
-	protected ArrayList<Integer> integersListValidation(String str) {
-		int numOfPiece = 0;
-		ArrayList<Integer> listOfIntegers = new ArrayList<>();
-		
-		String[] afterSplit = str.trim().split(" ");
-		int idNum;
-		try {
-			int validId = idNumberValidation(Integer.parseInt(afterSplit[0]));
-			    if (validId != -1) {
-			    	listOfIntegers.add(validId);
-			    }
-			    
-		} catch (NumberFormatException e1) {
-			// TO DO write error to file .
-			e1.printStackTrace();
-		}
-		
-		for (int i = 1; i < afterSplit.length; i++) {
-			if (afterSplit[i].equals("-1") || afterSplit[i].equals("0") || afterSplit[i].equals("1")) {
-				int currNum;
-				try {
-					currNum = Integer.parseInt(afterSplit[i]);
-				} catch (Exception e) {
-					throw new NumberFormatException();
-					// TO DO write error to file .
-				}
-				listOfIntegers.add(currNum);
-			} else {
-				listOfIntegers.clear();
-				// TO DO write error to file .
-				break;
-				
-			}
-		}
 		return listOfIntegers;
 	}
 
 	/**
+	 * Method build the single puzzle piece and add him to list.
+	 * 
 	 * @param listOfvalidintegers
-	 * @return
+	 * @return listOfPuzzlePieces
 	 */
 	protected ArrayList<PuzzlePiece> PuzzlePieceBuilder(ArrayList<Integer> listOfvalidintegers) {
 
@@ -150,9 +181,21 @@ public class FileDataValidation {
 		return listOfPuzzlePieces;
 	}
 
+	/**
+	 * @param list
+	 * @return  true/false
+	 */
+	protected boolean basicFileValidator(ArrayList<String> inputlist) {
+
+		if (inputlist.size() <= 2) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	public void setNumOfPieces(int numOfPieces) {
 		this.numOfPieces = numOfPieces;
 	}
-	
-	
+
 }
--- Puzzle-BIQ/src/test/java/course/puzzle/file/FileReaderTest.java
+++ Puzzle-BIQ/src/test/java/course/puzzle/file/FileReaderTest.java
@@ -16,37 +16,61 @@ import course.puzzle.puzzle.PuzzlePiece;
 public class FileReaderTest {
 	@Ignore
 	@Test
-	public void fileReaderVerification() throws IOException {
-		PuzzlePiece piece1 = new PuzzlePiece(1, -1, 0, 0, 0);
-		PuzzlePiece piece2 = new PuzzlePiece(2, 0, 0, 0, 0);
-		ArrayList<PuzzlePiece> expected = new ArrayList<>();
-		expected.add(piece1);
-		expected.add(piece2);
-
+	public void goodValidatorIntegrationFlow() throws IOException  {
 		String fromPath = "src/main/resources/files/puzzle1";
 		FileReader file = new FileReader();
-		ArrayList actual = file.readFromFile(fromPath);
-
+		ArrayList rawData = file.readFromFile(fromPath);		
 		FileDataValidation validator = new FileDataValidation();
+		ArrayList<PuzzlePiece> actualPiecesList = validator.fileDataValidator(rawData);
 
-		ArrayList<PuzzlePiece> pieces = validator.fileDataValidator(actual);
-
-		assertTrue(expected.equals(pieces));
+		ArrayList<PuzzlePiece> expectedPiecesList = new ArrayList<>();
+		expectedPiecesList.add(new PuzzlePiece(1, -1, 0, 0, 0));
+		expectedPiecesList.add(new PuzzlePiece(2, 0, 0, 0, 0));
+		
+		assertEquals(expectedPiecesList,actualPiecesList);
 	}
 
 	@Ignore
 	@Test
-	public void goodTestOFfirstLineValidator() {
+	public void goodTestOFfirstLineValidator() throws IOException {
 		int expected = 2;
 		FileDataValidation validator = new FileDataValidation();
 		String str = "NumElements=2";
 		int actual = validator.firstLineValidator(str);
 		assertEquals(expected, actual);
 	}
-
 	@Ignore
 	@Test
-	public void badTestOFfirstLineValidatorWringNumOfPieces_0() {
+	public void goodTestOFfirstLineValidatorIncludedSpaces() throws IOException {
+		int expected = 2;
+		FileDataValidation validator = new FileDataValidation();
+		String str = "NumElem ents=    2";
+		int actual = validator.firstLineValidator(str);
+		assertEquals(expected, actual);
+	}
+	@Ignore
+	@Test
+	public void goodTestOFfirstLineValidatorIncludedSpaces1() throws IOException {
+		int expected = 2;
+		FileDataValidation validator = new FileDataValidation();
+		String str = "NumElem ents=2";
+		int actual = validator.firstLineValidator(str);
+		assertEquals(expected, actual);
+		
+	}
+	@Ignore
+	@Test
+	public void badTestOFfirstLineValidatorInvalidChar_$() throws IOException {
+		int expected = -1;
+		FileDataValidation validator = new FileDataValidation();
+		String str = "NumElem ents=$";
+		int actual = validator.firstLineValidator(str);
+		assertEquals(expected, actual);
+		
+	}
+	@Ignore
+	@Test
+	public void badTestOFfirstLineValidatorIncorrectNumOfPieces_0() throws IOException {
 		int expected = -1;
 		FileDataValidation validator = new FileDataValidation();
 		String str = "NumElements=0";
@@ -56,7 +80,7 @@ public class FileReaderTest {
 
 	@Ignore
 	@Test
-	public void badTestOFfirstLineValidatorWringNumOfPieces_1() {
+	public void badTestOFfirstLineValidatorIncorrectNumOfPieces_1() throws IOException {
 		int expected = -1;
 		FileDataValidation validator = new FileDataValidation();
 		String str = "NumElements=1";
@@ -66,17 +90,16 @@ public class FileReaderTest {
 
 	@Ignore
 	@Test
-	public void badTestOFfirstLineValidatorWrongStringFormat() {
+	public void badTestOFfirstLineValidatorWrongStringFormat() throws IOException {
 		int expected = -1;
 		FileDataValidation validator = new FileDataValidation();
 		String str = "Num$$Elements=0";
 		int actual = validator.firstLineValidator(str);
 		assertEquals(expected, actual);
 	}
-
 	@Ignore
 	@Test
-	public void goodTestIntegersListValidation() {
+	public void goodTestIntegersListValidation() throws IOException {
 		ArrayList<Integer> expectedList = new ArrayList<>();
 		expectedList.add(1);
 		expectedList.add(-1);
@@ -85,28 +108,62 @@ public class FileReaderTest {
 		expectedList.add(0);
 		FileDataValidation validator = new FileDataValidation();
 		String secondLine = "1 -1 0 0 0";
-		String firstLine = "NumElements=2";
-		validator.firstLineValidator(firstLine);
+		validator.setNumOfPieces(2);
 		ArrayList<Integer> actualList = validator.integersListValidation(secondLine);
 		assertEquals(expectedList, actualList);
 
 	}
 
-	@Ignore
+	
 	@Test
-	public void badTestIntegersListValidation() {
+	public void badTestIntegersListValidation() throws IOException {
 		ArrayList<Integer> expectedList = new ArrayList<>();
-		expectedList.clear();
+		expectedList.add(1);
+		expectedList.add(-1);
+		expectedList.add(0);
+		expectedList.add(0);
+		
 		FileDataValidation validator = new FileDataValidation();
+		validator.setNumOfPieces(1);
 		String str = "1 -1 3 0 0";
 		ArrayList<Integer> actualList = validator.integersListValidation(str);
 		assertEquals(expectedList, actualList);
 
 	}
-
 	@Ignore
 	@Test
-	public void goodTestIdNumberValidation() {
+	public void badTestIntegersListValidationMoreThen5Elements() throws IOException {
+		ArrayList<Integer> expectedList = new ArrayList<>();
+		expectedList.clear();
+		FileDataValidation validator = new FileDataValidation();
+		String str = "1 -1 3 0 0 0";
+		ArrayList<Integer> actualList = validator.integersListValidation(str);
+		assertEquals(expectedList, actualList);
+	}
+	@Ignore
+	@Test
+	public void badTestIntegersListValidationInvaldChar() throws IOException {
+		ArrayList<Integer> expectedList = new ArrayList<>();
+		expectedList.clear();
+		FileDataValidation validator = new FileDataValidation();
+		String str = "1 -1 3 0 b ";
+		ArrayList<Integer> actualList = validator.integersListValidation(str);
+		assertEquals(expectedList, actualList);
+	}
+	@Ignore
+	@Test
+	public void badTestIntegersListValidationInvaldChar_ID() throws IOException {
+		ArrayList<Integer> expectedList = new ArrayList<>();
+		expectedList.clear();
+		FileDataValidation validator = new FileDataValidation();
+		String str = "b -1 3 0 0 ";
+		ArrayList<Integer> actualList = validator.integersListValidation(str);
+		assertEquals(expectedList, actualList);
+	}
+	
+	@Ignore
+	@Test
+	public void goodTestIdNumberValidation() throws IOException {
 		int expected = 1;
 		FileDataValidation validator = new FileDataValidation();
 		int idNum = 1;
@@ -126,10 +183,9 @@ public class FileReaderTest {
 		assertEquals(expected, actual);
 
 	}
-
 	@Ignore
 	@Test
-	public void badTestIdNumberValidation_BadIdNumber_Negative() {
+	public void badTestIdNumberValidation_BadIdNumber_NegativeNumber() {
 		int expected = -1;
 		FileDataValidation validator = new FileDataValidation();
 		int idNum = -1;
@@ -137,7 +193,7 @@ public class FileReaderTest {
 		assertEquals(expected, actual);
 
 	}
-
+	@Ignore
 	@Test
 	public void goodPuzzlePieceBuilder() {
 		FileDataValidation validator = new FileDataValidation();
@@ -152,7 +208,7 @@ public class FileReaderTest {
 		ArrayList<PuzzlePiece> actual = validator.PuzzlePieceBuilder(listOfIntegers);
 		assertEquals(expected.get(0).getId(), actual.get(0).getId());
 	}
-	
+	@Ignore
 	@Test
 	public void badPuzzlePieceBuilder() {
 		FileDataValidation validator = new FileDataValidation();
@@ -166,5 +222,61 @@ public class FileReaderTest {
 		listOfIntegers.add(0);
 		ArrayList<PuzzlePiece> actual = validator.PuzzlePieceBuilder(listOfIntegers);
 		assertNotEquals(expected.get(0).getId(), actual.get(0).getId());
+	}
+	@Ignore
+	@Test 
+	public void goodfileDataValidator() throws IOException {
+			
+		ArrayList<String> input = new ArrayList<>();
+		input.add("NumElements=2");
+		input.add("1 -1 0 0 0");
+		input.add("2 0 0 0 0");
+		
+		PuzzlePiece piece1 = new PuzzlePiece(1, -1, 0, 0, 0);
+		PuzzlePiece piece2 = new PuzzlePiece(2, 0, 0, 0, 0);
+		ArrayList<PuzzlePiece> expected = new ArrayList<>();
+		expected.add(piece1);
+		expected.add(piece2);
+		
+		FileDataValidation validator = new FileDataValidation();
+		ArrayList<PuzzlePiece> actual = validator.fileDataValidator(input);
+		assertEquals(expected.get(0).getId(), actual.get(0).getId());
+		
+	}
+	@Ignore
+	@Test 
+	public void badfileDataValidator1() throws IOException {
+			
+		ArrayList<String> input = new ArrayList<>();
+		input.add("NumElements=2");
+		input.add("1 -1 0 0 0");
+		// the id more then total elements.
+		input.add("3 0 0 0 0");
+		
+		ArrayList<PuzzlePiece> expected = new ArrayList<>();
+		
+		FileDataValidation validator = new FileDataValidation();
+		ArrayList<PuzzlePiece> actual = validator.fileDataValidator(input);
+		// Empty list was received .
+		assertEquals(expected, actual);
+		
+	}
+	@Ignore
+	@Test 
+	public void badfileDataValidator2() throws IOException {
+			
+		ArrayList<String> input = new ArrayList<>();
+		// NumElements more then actual received.
+		input.add("NumElements=3");
+		input.add("1 -1 0 0 0");
+		input.add("2 0 0 0 0");
+		
+		ArrayList<PuzzlePiece> expected = new ArrayList<>();
+		
+		FileDataValidation validator = new FileDataValidation();
+		ArrayList<PuzzlePiece> actual = validator.fileDataValidator(input);
+		// Empty list was received .
+		assertEquals(expected, actual);
+		
 	}
 }
