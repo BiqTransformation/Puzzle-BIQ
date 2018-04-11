107c567a54b627db5217cdca3fe34fffa57341f5
Shestakov, Svetlana (ss223m) <ss223m@intl.att.com>
2018-04-11 16:04:39 +0300

    [Svetlana] - puzzle solution completed, need to be tested

--- Puzzle-BIQ/src/test/java/course/puzzle/e2e/E2ETests.java
+++ Puzzle-BIQ/src/test/java/course/puzzle/e2e/E2ETests.java
@@ -14,7 +14,7 @@ import course.puzzle.puzzle.PuzzlePiece;
 import course.puzzle.puzzle.SolvePuzzle;
 
 public class E2ETests {
-
+private String filesPath = "src//test//resources//files//";
 	
 	@Test
 	public void testSimpleE2EWith1ElementPuzzle() throws Exception{
@@ -35,26 +35,67 @@ public class E2ETests {
 	}
 	
 	@Test
-	public void testSimpleE2EWith1E4ementPuzzle() throws Exception{
-		String fromPath = "src//test//resources//files//test2.in";
-		String toPath = "src//test//resources//files//test2.out";
-		//clean output
-		FileOutput.path=toPath;
-	    FileOutput.cleanOutputFile();	    
-		List<String> inputList =FileReader.readFromFile(fromPath);	
+	public void test3PiecesPuzzle() throws Exception{
+		String input = filesPath + "good3Pieces.in";
+		String output = filesPath + "good3Pieces.out";
+//		//clean output
+		FileOutput.path=output;
+	    FileOutput.cleanOutputFile();
+		List<String> inputList =FileReader.readFromFile(input);
 		FileDataValidation fav = new FileDataValidation();
 		List<PuzzlePiece> puzzleList= fav.fileDataValidator(inputList);
 		SolvePuzzle sp = new SolvePuzzle(puzzleList);
 		PuzzlePiece[][] puz =sp.findSolution();
-		//TODO - SolvePuzzle should print
+
 		FileOutput.printSolution(puz);		
 		String message = FileOutput.loadFromTextFile();	
 		System.out.println(message);
+		assertTrue(message.contains("3 1 2"));
+
+	}
+
+	@Test
+	public void test4PiecesPuzzle() throws Exception{
+		String input = filesPath + "good4Pieces.in";
+		String output = filesPath + "good4Pieces.out";
+//		//clean output
+		FileOutput.path=output;
+		FileOutput.cleanOutputFile();
+		List<String> inputList =FileReader.readFromFile(input);
+		FileDataValidation fav = new FileDataValidation();
+		List<PuzzlePiece> puzzleList= fav.fileDataValidator(inputList);
+		SolvePuzzle sp = new SolvePuzzle(puzzleList);
+		PuzzlePiece[][] puz =sp.findSolution();
+
+		FileOutput.printSolution(puz);
+		String message = FileOutput.loadFromTextFile();
+		System.out.println(message);
 		assertTrue(message.contains("1 3"));
 		assertTrue(message.contains("2 4"));
-		//wait till full solution of recursion
-	}  
-	
+
+	}
+	@Test
+	public void test9PiecesPuzzle() throws Exception{
+		String input = filesPath + "good9Pieces.in";
+		String output = filesPath + "good9Pieces.out";
+//		//clean output
+		FileOutput.path=output;
+		FileOutput.cleanOutputFile();
+		List<String> inputList =FileReader.readFromFile(input);
+		FileDataValidation fav = new FileDataValidation();
+		List<PuzzlePiece> puzzleList= fav.fileDataValidator(inputList);
+		SolvePuzzle sp = new SolvePuzzle(puzzleList);
+		PuzzlePiece[][] puz =sp.findSolution();
+
+		FileOutput.printSolution(puz);
+		String message = FileOutput.loadFromTextFile();
+		System.out.println(message);
+		assertTrue(message.contains("8 6 3"));
+		assertTrue(message.contains("9 5 2"));
+		assertTrue(message.contains("7 1 4"));
+	}
+
+
 	@Test
 	public void testSimpleNegativeE2EWithLessIDsThenNumElements() throws Exception{
 		String fromPath = "src//test//resources//files//test3.in";
