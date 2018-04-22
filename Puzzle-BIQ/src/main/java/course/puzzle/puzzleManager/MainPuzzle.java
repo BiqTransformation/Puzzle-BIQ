da04182149bc0cc6786ad4f8ef5905a600df573f
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-04-22 23:39:12 +0300

    [Lior] --------------------- adding TODO to classes

--- Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
@@ -9,6 +9,7 @@ import course.puzzle.puzzle.PuzzlePiece;
 public class MainPuzzle {
 
 	public static void main(String[] args) throws Exception {
+		//TODO- add parameter for rotate and number of threads - Alex
 		String fromPath = args[0];//"src//test//resources//files//test3.in";
 		String toPath = args[1];//"src//test//resources//files//test3.out";
 		PuzzleManager pm = new PuzzleManager(fromPath,toPath);
