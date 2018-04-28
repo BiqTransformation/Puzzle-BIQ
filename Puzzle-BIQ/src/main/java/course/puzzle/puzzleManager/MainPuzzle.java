f136ded80bbc46fe5ae798c47afe6520a4874be6
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-04-28 20:45:42 +0300

    [Lior] --------------------------------- fix main function

--- Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
@@ -10,16 +10,21 @@ public class MainPuzzle {
 
 	public static void main(String[] args) throws Exception {
 		
-		
+		int numOfThreads =0;
 		String fromPath = args[0];//"src//test//resources//files//test3.in";
 		String toPath = args[1];//"src//test//resources//files//test3.out";
-		String rotate = args[2];
+		String rotate = args[2];		
 		boolean rotBoolean = Boolean.parseBoolean(rotate);
-		String treads = args[3];
-		int numOfTreads = Integer.parseInt(treads);
+		String threads = args[3] != null ? args[3] : "4";	
+		
+		try{
+			numOfThreads  = Integer.parseInt(threads);
+		}
+		catch(NumberFormatException e){};
 		
 		
-		PuzzleManager pm = new PuzzleManager(fromPath,toPath,rotBoolean,numOfTreads);
+		
+		PuzzleManager pm = new PuzzleManager(fromPath,toPath,rotBoolean,numOfThreads);
 		pm.handlePuzzle();
 		FileOutput fo = new FileOutput(toPath);
 		String message = fo.loadFromTextFile();	
