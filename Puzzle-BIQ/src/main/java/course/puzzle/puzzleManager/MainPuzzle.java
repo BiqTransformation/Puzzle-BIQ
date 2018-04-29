6c626ead103a2e51c2ee639ab3f120b6f0bc97dd
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-04-29 22:37:19 +0300

    [Lior] --------------------------------- fix main to choose cmd parameters dynamically

--- Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
@@ -7,31 +7,76 @@ import course.puzzle.file.FileOutput;
 import course.puzzle.puzzle.PuzzlePiece;
 
 public class MainPuzzle {
+	private final static String INPUT = "-input";
+	private final static String OUTPUT = "-output";
+	private final static String ROTATE = "-rotate";
+	private final static String THREADS = "-threads";
 
 	public static void main(String[] args) throws Exception {
-		
-		int numOfThreads =0;
-		String fromPath = args[0];//"src//test//resources//files//test3.in";
-		String toPath = args[1];//"src//test//resources//files//test3.out";
-		String rotate = args[2];		
-		boolean rotBoolean = Boolean.parseBoolean(rotate);
-		String threads = args[3] != null ? args[3] : "4";	
-		
-		try{
-			numOfThreads  = Integer.parseInt(threads);
-		}
-		catch(NumberFormatException e){};
+		 String fromPath = getInputFile(args);
+		 String toPath = getOutputFile(args);
+		 boolean isRotate= isRotate(args);
+		 System.out.println(isRotate);
+		 int numOfThreads = getNumOfThreads(args);
+		 System.out.println(numOfThreads);
 		
 		
 		
-		PuzzleManager pm = new PuzzleManager(fromPath,toPath,rotBoolean,numOfThreads);
+		PuzzleManager pm = new PuzzleManager(fromPath,toPath,isRotate,numOfThreads);
 		pm.handlePuzzle();
 		FileOutput fo = new FileOutput(toPath);
 		String message = fo.loadFromTextFile();	
-		System.out.println(message);
+		//System.out.println(message);
 		
 	
 
 	}
 
+	private static int getNumOfThreads(String[] args) {
+		int numOfThreads = 0;
+		for(String str :args){
+			if(str.startsWith(THREADS)){
+				try{
+					 numOfThreads = Integer.parseInt(str.substring(9));
+					}
+				catch(NumberFormatException e){}
+				return numOfThreads;
+			}
+		}
+		return 4;
+	}
+
+	private static boolean isRotate(String[] args) {
+		for(String str : args){
+			if(str.equals(ROTATE)){
+				return true;
+			}
+			
+		}
+		return false;
+	}
+
+	private static String getOutputFile(String[] args) {
+		String toPath = "";
+		for(String str : args){
+			if(str.startsWith(OUTPUT)){
+				toPath =str.substring(8,str.length());
+				//System.out.println(toPath);
+			}
+		}
+		return toPath;
+	}
+		
+	
+
+	private static String getInputFile(String[] args) {	
+		String fromPath = "";
+		for(String str : args){
+			if(str.startsWith(INPUT)){
+				fromPath =str.substring(7,str.length());
+				//System.out.println(fromPath);
+			}
+		}
+		return fromPath;
+	}
 }
