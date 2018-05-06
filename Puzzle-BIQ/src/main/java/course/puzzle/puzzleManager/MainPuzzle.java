939bc8f57e02e7ac76586930a1723f36c0dec77a
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-05-06 22:44:30 +0300

    [Lior] ----------------change MainPuzzle

--- Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
+++ Puzzle-BIQ/src/main/java/course/puzzle/puzzleManager/MainPuzzle.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import course.puzzle.file.FileOutput;
 import course.puzzle.puzzle.PuzzlePiece;
-
+/*
+ * @author Alex
+ */
 public class MainPuzzle {
 	private final static String INPUT = "-input";
 	private final static String OUTPUT = "-output";
@@ -16,9 +18,9 @@ public class MainPuzzle {
 		 String fromPath = getInputFile(args);
 		 String toPath = getOutputFile(args);
 		 boolean isRotate= isRotate(args);
-		 System.out.println(isRotate);
+		 //System.out.println(isRotate);
 		 int numOfThreads = getNumOfThreads(args);
-		 System.out.println(numOfThreads);
+		 //System.out.println(numOfThreads);
 		
 		
 		
@@ -26,7 +28,7 @@ public class MainPuzzle {
 		pm.handlePuzzle();
 		FileOutput fo = new FileOutput(toPath);
 		String message = fo.loadFromTextFile();	
-		//System.out.println(message);
+		System.out.println(message);
 		
 	
 
