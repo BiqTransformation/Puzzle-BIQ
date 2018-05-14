08ddae20df08e9cc8bc2b40629dcd83c52febcea
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-05-14 16:47:28 +0300

    [Lior]------------------------ refactor tests and chabge code

--- Puzzle-BIQ/src/test/java/course/puzzle/puzzle/PossibleSolutionsTest.java
+++ Puzzle-BIQ/src/test/java/course/puzzle/puzzle/PossibleSolutionsTest.java
@@ -15,7 +15,7 @@ public class PossibleSolutionsTest {
 	List<PuzzlePiece> puzzle = new ArrayList<>();
 
 	@Test
-	public void testgetPossiblesolutionWith6PuzzlePieces() throws Exception {
+	public void testgetPossiblesolutionWith6PuzzlePiecesNoRotate() throws Exception {
 		PuzzlePiece piece1 = new PuzzlePiece(1, 0, 0, 1, 0);
 		PuzzlePiece piece2 = new PuzzlePiece(2, -1, 0, -1, 0);
 		PuzzlePiece piece3 = new PuzzlePiece(3, 1, 0, 0, 0);
@@ -29,22 +29,24 @@ public class PossibleSolutionsTest {
 		puzzle.add(piece4);
 		puzzle.add(piece5);
 		puzzle.add(piece6);
-		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,rotate),numOfThreads);
+		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,false),numOfThreads);
 		Map<Integer, Integer> mapUnderTest = solve.getPossibleSolutions();
-		assertTrue(mapUnderTest.size()==0);
+		Map<Integer,Integer> expected = new HashMap<>();
+		expected.put(1,6);
+		expected.put(2,3);
+		System.out.println(mapUnderTest);
+		assertEquals(expected,mapUnderTest);
 	}
-
-
+	
+	
 	@Test
-	public void testgetPossiblesolutionWith8PuzzlePieces() throws Exception {
+	public void testgetPossiblesolutionWith6PuzzlePiecesRotate() throws Exception {
 		PuzzlePiece piece1 = new PuzzlePiece(1, 0, 0, 1, 0);
 		PuzzlePiece piece2 = new PuzzlePiece(2, -1, 0, -1, 0);
 		PuzzlePiece piece3 = new PuzzlePiece(3, 1, 0, 0, 0);
 		PuzzlePiece piece4 = new PuzzlePiece(4, 0, 0, -1, 0);
 		PuzzlePiece piece5 = new PuzzlePiece(5, 1, 0, 1, 0);
-		PuzzlePiece piece6 = new PuzzlePiece(6, -1, 0, 1, 0);
-		PuzzlePiece piece7 = new PuzzlePiece(7, -1, 0, -1, 0);
-		PuzzlePiece piece8 = new PuzzlePiece(8, 1, 0, 0, 0);
+		PuzzlePiece piece6 = new PuzzlePiece(6, -1, 0, 0, 0);
 		puzzle.clear();
 		puzzle.add(piece1);
 		puzzle.add(piece2);
@@ -52,53 +54,19 @@ public class PossibleSolutionsTest {
 		puzzle.add(piece4);
 		puzzle.add(piece5);
 		puzzle.add(piece6);
-		puzzle.add(piece7);
-		puzzle.add(piece8);
-		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,rotate),numOfThreads);
+		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,true),numOfThreads);
 		Map<Integer, Integer> mapUnderTest = solve.getPossibleSolutions();
-		assertTrue(mapUnderTest.size()==0);
-		
-	}
-
-	@Test
-	public void testgetPossiblesolutionWith3PuzzlePieces() throws Exception {
-		PuzzlePiece piece1 = new PuzzlePiece(1, 0, 0, 1, 0);
-		PuzzlePiece piece2 = new PuzzlePiece(2, -1, 0, -1, 0);
-		PuzzlePiece piece3 = new PuzzlePiece(3, 1, 0, 0, 0);
-		puzzle.clear();
-		puzzle.add(piece1);
-		puzzle.add(piece2);
-		puzzle.add(piece3);
-		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,rotate),numOfThreads);
-		Map<Integer, Integer> mapUnderTest = solve.getPossibleSolutions();
-		System.out.println(mapUnderTest);	
-		assertTrue(mapUnderTest.size()==0);
-	}
-
-	@Test
-	public void testgetPossiblesolutionWith1PuzzlePieces() throws Exception {
-		PuzzlePiece piece1 = new PuzzlePiece(1, 0, 0, 0, 0);
-		puzzle.clear();
-		puzzle.add(piece1);
-		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,rotate),numOfThreads);
-		Map<Integer, Integer> mapUnderTest = solve.getPossibleSolutions();
-		Map<Integer, Integer> testMap = new HashMap<>();
-		testMap.put(1, 1);
-		assertEquals(mapUnderTest, testMap);
-	}
-
-	@Test
-	public void testgetPossiblesolutionWith2PuzzlePieces() throws Exception {
-		PuzzlePiece piece1 = new PuzzlePiece(1, 0, 0, -1, 0);
-		PuzzlePiece piece2 = new PuzzlePiece(2, 1, 0, 0, 0);
-		puzzle.clear();
-		puzzle.add(piece1);
-		puzzle.add(piece2);
-		PuzzleSolver solve = new PuzzleSolver(new Puzzle(puzzle,rotate),numOfThreads);
-		Map<Integer, Integer> mapUnderTest = solve.getPossibleSolutions();
+		Map<Integer,Integer> expected = new HashMap<>();
+		expected.put(1,6);
+		expected.put(2,3);
 		System.out.println(mapUnderTest);
-		assertTrue(mapUnderTest.size()==0);
+		assertEquals(expected,mapUnderTest);
 	}
+
+
+
+
+
 
 
 }
